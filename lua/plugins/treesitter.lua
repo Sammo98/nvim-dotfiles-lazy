cc3920d1ea3cf3ab8d1675abbc87956ab8ae2023
Sam Rees <sam.rees@gmail.com>
2023-04-16 14:25:54 +0100

    treesitter changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,73 +1,61 @@
 return {
-  { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
-
-  {
-    "mfussenegger/nvim-treehopper",
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    event = "BufReadPre",
-    config = true,
-  },
-
+  -- Syntax highlightings
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "diff",
-        "fish",
-        "gitignore",
-        "help",
-        "html",
-        "http",
-        "javascript",
-        "jsdoc",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "rust",
-        "scss",
-        "sql",
-        "svelte",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-        "json",
-      },
-      matchup = {
-        enable = true,
-      },
-      highlight = { enable = true },
-      query_linter = {
-        enable = true,
-        use_virtual_text = true,
-        lint_events = { "BufWrite", "CursorHold" },
-      },
-      playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = true, -- Whether the query persists across vim sessions
-        keybindings = {
-          toggle_query_editor = "o",
-          toggle_hl_groups = "i",
-          toggle_injected_languages = "t",
-          toggle_anonymous_nodes = "a",
-          toggle_language_display = "I",
-          focus_language = "f",
-          unfocus_language = "F",
-          update = "R",
-          goto_node = "<cr>",
-          show_help = "?",
-        },
-      },
+    build = ":TSUpdate",
+    event = "BufEnter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "nvim-treesitter/playground",
+      "windwp/nvim-ts-autotag",
     },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+          disable = { "python" },
+        },
+        ensure_installed = {
+          'vim',
+          'markdown',
+          'markdown_inline',
+          'bash',
+          'regex',
+          'gomod',
+          'javascript',
+          'typescript',
+          'tsx',
+          'json',
+          'toml',
+          'html',
+          'css',
+          'scss',
+          'lua',
+          'rust'
+        },
+        auto_install = true,
+        autotag = { -- dependency with 'nvim-ts-autotag'
+          enable = true
+        },
+        playground = {
+          enable = true,
+          disable = {},
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+            keymaps = {
+            },
+          },
+        },
+      }
+      )
+    end
   },
+
 }
